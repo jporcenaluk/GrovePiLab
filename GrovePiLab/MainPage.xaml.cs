d50afe67b2524ed9ef75b7e4f0114a88838f3718
Jared Porcenaluk <jared@NebbiaTech.com>
2017-03-29 17:01:03 -0400

    Part four is done!

--- GrovePiLab/MainPage.xaml.cs
+++ GrovePiLab/MainPage.xaml.cs
@@ -17,6 +17,8 @@ using GrovePi.Sensors;
 using GrovePi.I2CDevices;
 using System.Diagnostics;
 using System.Threading.Tasks;
+using Microsoft.Azure.Devices.Client;
+using System.Text;
 
 namespace GrovePiLab
 {
@@ -25,11 +27,19 @@ namespace GrovePiLab
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        // This is a default place holder connection string for the lab. 
+        // During Day 3 lab, you will replace the string with your own connection string once you 
+        // have created your own IoT Hub and device on the hub.
+        const string deviceConnectionString = "HostName=VirtualLabIotHub.azure-devices.net;DeviceId=6833085;SharedAccessKey=ku1xGWBust/Lc4bKvORQnAHklPCAtsrN/s830cGsvdY=";
+
+
         public MainPage()
         {
             this.InitializeComponent();
             InitializeGrove();
+            InitDeviceClient();
             StartTelemetry();
+            InitAzureIotReceiver();
         }
 
         IRgbLcdDisplay display;
@@ -50,7 +60,7 @@ namespace GrovePiLab
                 SetLcdText("You did it bro.");
                 GroveStatus.Text = "Grove gone done and initted.";
             }
-            catch(Exception e)
+            catch (Exception e)
             {
                 Debug.WriteLine("Grove not found :" + e.ToString());
                 GroveStatus.Text = "Grove not found.";
@@ -113,7 +123,7 @@ namespace GrovePiLab
                 //Some random number with +/- ranges
                 Temperature = Math.Ceiling((90 + random.NextDouble() * 4 - 2) * 10) / 10;
                 Humidity = Math.Ceiling((70 + random.NextDouble() * 4 - 2) * 10) / 10;
-                WindSpeed = Math.Ceiling((60 + random.NextDouble() * 4 - 2) * 10) / 10; 
+                WindSpeed = Math.Ceiling((60 + random.NextDouble() * 4 - 2) * 10) / 10;
             }
         }
 
@@ -130,7 +140,7 @@ namespace GrovePiLab
                     data.Humidity = sensor.Humidity;
                     data.LiveData = true;
                 }
-                catch(Exception e)
+                catch (Exception e)
                 {
                     Debug.WriteLine(e.ToString());
                 }
@@ -163,8 +173,92 @@ namespace GrovePiLab
                         GroveStatus.Text = String.Format("[{0}] Last Telemetry: {1}", msgCount++, msgString);
                     });
 
+                    await SendDeviceToCloudMessageAsync(msgString);
+
                     //Wait a second between every measurement
                     Task.Delay(1000).Wait();
+                }
+            });
+        }
+
+        DeviceClient deviceClient;
+        public void InitDeviceClient()
+        {
+            deviceClient = DeviceClient.CreateFromConnectionString(deviceConnectionString, TransportType.Http1);
+
+        }
+
+        public async Task SendDeviceToCloudMessageAsync(string messageText)
+        {
+            try
+            {
+                var message = new Message(Encoding.ASCII.GetBytes(messageText));
+                await deviceClient.SendEventAsync(message);
+
+                Debug.WriteLine("Sent message " + messageText);
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine("SendDeviceToCloudMessageAsync error = : " + e.ToString());
+            }
+        }
+
+        public async Task<string> ReceiveCloudToDeviceMessageAsync()
+        {
+            while (true)
+            {
+                try
+                {
+                    var receiveMessage = await deviceClient.ReceiveAsync();
+                    if (receiveMessage != null)
+                    {
+                        var messageData = Encoding.ASCII.GetString(receiveMessage.GetBytes());
+                        await deviceClient.CompleteAsync(receiveMessage);
+                        return messageData;
+                    }
+
+                }
+                catch (Exception e)
+                {
+                    Debug.WriteLine("Error in ReceiveCloudToDeviceMessageAsync: " + e.ToString());
+                }
+
+                await Task.Delay(1000);
+            }
+        }
+
+        private void InitAzureIotReceiver()
+        {
+            Task.Run(async () =>
+            {
+                while (true)
+                {
+                    string receivedMessage = await ReceiveCloudToDeviceMessageAsync();
+                    if (receivedMessage == null) continue;
+                    Debug.WriteLine("Received message = " + receivedMessage);
+
+                    switch (receivedMessage.ToLower())
+                    {
+                        case "green":
+                            {
+                                await Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.High, () =>
+                                {
+                                    lcdIsGreen = true;
+                                    SetLcdGreen(lcdIsGreen);
+                                });
+                                break;
+                            }
+                        case "red":
+                            {
+                                await Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.High, () =>
+                                {
+                                    lcdIsGreen = false;
+                                    SetLcdGreen(lcdIsGreen);
+                                });
+                                break;
+                            }
+                        default: { break; }
+                    }
                 }
             });
         }
